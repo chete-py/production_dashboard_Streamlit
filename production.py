27eeed0e730d8d8faf7e06f9ca415eac388ed90a
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-12-10 12:45:47 +0300

    Update production.py

--- production.py
+++ production.py
@@ -102,7 +102,7 @@ if uploaded_file is not None:
 
         st.plotly_chart(fig)
 
-        AgGrid(preview_sorted)
+        st.write(preview_sorted)
 
     if view == 'Branch':
         unique = newdf['BRANCH'].unique()
